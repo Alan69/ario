394f33c95f0529d99d2a6ef022595741f3c9c96c
Alan69 <gameg1676@gmail.com>
2022-12-15 14:45:52 +0600

    RequestTypeThree to admin

--- requestapp/admin.py
+++ requestapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Request, RequestTypeTwo
+from .models import Request, RequestTypeTwo, RequestTypeThree
 # Register your models here.
 admin.site.register(Request)
 admin.site.register(RequestTypeTwo)
+admin.site.register(RequestTypeThree)
